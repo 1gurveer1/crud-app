444109d6b20f69c4b52c9169f73218e1006c46e0
1gurveer1 <64747174+1gurveer1@users.noreply.github.com>
2022-01-04 23:36:32 +0530

    Add files via upload

---
+++ backend/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const mongoURI = "mongodb://localhost:27017/eNote?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+
+const connectToMongo = () => {
+    mongoose.connect(mongoURI, ()=>{
+        console.log("Connected to MongoDB")
+    })
+}
+
+module.exports = connectToMongo;
+
